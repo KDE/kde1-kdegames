289b2e54497ef3474ad6644a813fef3c918e35ee
KDE Dinosaur kulow <cvsuser@kde.org>
1997-09-14 16:33:43 +0000

    corrected all games kdelnk files and added some german translations the submenu Games look now very good (no k* anymore) fixed some paths in applications

--- kpoker/kpoker.cpp
+++ kpoker/kpoker.cpp
@@ -60,7 +60,7 @@ kpok::kpok(QWidget *parent, const char *name)
 	setCaption( version );
 	
 	
-	QString bitmapdir = KPoker->kdedir() + QString("/share/apps/kpoker/pics");
+	QString bitmapdir = KPoker->kdedir() + QString("/share/apps/kpoker/pics/");
 	
 	
 	/* KKeyCode initialization */
