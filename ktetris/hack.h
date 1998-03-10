53a7c52ecbc4d0e24d7293e4c4f30480ef84fbf1
KDE Dinosaur hadacek <cvsuser@kde.org>
1998-03-10 22:55:10 +0000

    *** empty log message ***

--- ktetris/hack.h
+++
@@ -1,8 +0,0 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-
-/* if someone could explain me why this hack seems necessary to compile */
-
-int Connect(int sockfd, struct sockaddr *serv_addr, int addrlen);
-int Accept(int sock, struct sockaddr *addr, int *addrlen);
-                
